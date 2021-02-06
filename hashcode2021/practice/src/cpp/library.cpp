3f8fcf913790a135584e1865a6dbf8e704e25f2b
Shreeda Ghatpande <shreeda3@gmail.com>
2021-02-06 17:23:29 +0530

    Basic reader code

--- hashcode2021/practice/src/cpp/library.cpp
+++ hashcode2021/practice/src/cpp/library.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <cassert>
+#include <fstream>
+#include <vector>
 #include <list>
 
 using namespace std;
+
+class Library;
+class Book;
 
 class InputReader {
     public:
@@ -53,6 +59,50 @@ class Library {
         long unsigned int id;
         unsigned int signUpTime;
         unsigned int numBooksPerDay;
-        list<Book> totalBooks;
+        list<Book> sortedBooks;
 
 };
+
+static size_t split(const std::string &txt, std::vector<std::string> &strs, char ch)
+{
+    size_t pos = txt.find( ch );
+    size_t initialPos = 0;
+    strs.clear();
+
+    // Decompose statement
+    while( pos != std::string::npos ) {
+        strs.push_back( txt.substr( initialPos, pos - initialPos ) );
+        initialPos = pos + 1;
+
+        pos = txt.find( ch, initialPos );
+    }
+
+    // Add the last one
+    strs.push_back( txt.substr( initialPos, std::min( pos, txt.size() ) - initialPos + 1 ) );
+
+    return strs.size();
+}
+
+InputReader::InputReader(string inp_path){
+    filepath = inp_path;
+}
+  
+InputReader::~InputReader() {
+    // Empty
+}
+
+void InputReader::getMetaData(long unsigned int& numBooks, long unsigned int& numLibraries, long unsigned int& numDays) {
+    //  add a filepath assert here
+    ifstream metaFile;
+    metaFile.open(filepath);
+    string metadata;
+    metaFile >> metadata;
+    vector<string> data; 
+    size_t opsize = split(metadata, data, ' ');
+    assert(opsize==3);
+    numBooks = std::stoul(data[0]);
+    numLibraries = std::stoul(data[1]);
+    numDays = std::stoul(data[2]);
+    metaFile.close();
+
+} 
--- hashcode2021/practice/src/cpp/main.cpp
+++ hashcode2021/practice/src/cpp/main.cpp
@@ -2,18 +2,23 @@
 #include <string>
 #include <list>
 
+using namespace std;
+
 int main() {
-	string input_path = "";
+	string input_path = "/mnt/d/code/hashcode/CodingSolutions/hashcode2021/practice/sampleInput/a_example.txt";
 
 	long unsigned int numBooks, numLibraries, numDays;
 
 	InputReader inputReader(input_path);
 	inputReader.getMetaData(numBooks, numLibraries, numDays);
+    std::cout << "Details received are " <<numBooks << numLibraries << numDays << endl;
 
-	list<Library> libraries = inputReader.getLibraries();
+	// list<Library> libraries = inputReader.getLibraries();
 
-	list<Book> books = inputReader.getBooks();
+	// list<Book> books = inputReader.getBooks();
 
-	std::cout << "Hello World";
+    for (int i = 0; i < numDays; ++i) {
+        //
+    }
     return 0;
 }
