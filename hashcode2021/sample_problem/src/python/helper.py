88886b8d9108e16b7d37feeea186f394bfbfec0d
Shreeda Ghatpande <shreeda3@gmail.com>
2021-02-21 16:46:23 +0530

    Added pizza print logic and some formatting

--- hashcode2021/sample_problem/src/python/helper.py
+++ hashcode2021/sample_problem/src/python/helper.py
@@ -9,6 +9,8 @@ class Pizza:
         self.num = num_ingd
         self.ingredients = ingredients
 
+    def __str__(self):
+        return f"Pizza {self.id} ingredients list {self.ingredients}"
 
 ################################
 
@@ -31,15 +33,15 @@ def read_input(inp):
             num_ingd = int(p_details.pop(0))
             pizzas.append(Pizza(pizza_id, num_ingd, p_details))
     
-    
-    print(f"meta data is {meta}")
     meta = meta.split(' ')
-    return meta
+    return meta, pizzas
 
 
 if __name__ == '__main__':
-    meta = read_input('../../ref/a_example')
-    num_pizzas = meta[0]
-    num_2_member_team = meta[1]
-    num_3_member_team = meta[2]
-    num_4_member_team = meta[3]
+    meta, pizzas = read_input('../../ref/a_example')
+    print(f"Meta is {meta}, Pizza are {pizzas}")
+    
+    num_pizzas = int(meta[0])
+    num_2_member_team = int(meta[1])
+    num_3_member_team = int(meta[2])
+    num_4_member_team = int(meta[3])
